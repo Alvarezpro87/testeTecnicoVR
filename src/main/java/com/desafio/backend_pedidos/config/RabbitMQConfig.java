c15cae7e29ad45f8439cded232ba6335f813b625
Alvarezpro87 <alvareznuvem@gmail.com>
2025-07-31 19:24:12 -0300

    git commit -m "task: configurar exchange, routingKey e DLQ para a fila de pedidos"

--- src/main/java/com/desafio/backend_pedidos/config/RabbitMQConfig.java
+++ src/main/java/com/desafio/backend_pedidos/config/RabbitMQConfig.java
@@ -1,6 +1,6 @@
 package com.desafio.backend_pedidos.config;
 
-import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
@@ -10,11 +10,32 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class RabbitMQConfig {
 
-    public static final String FILA_PEDIDOS = "fila_pedidos";
+    public static final String FILA_PEDIDOS = "pedidos.entrada.alexandre";
+    public static final String DLQ_PEDIDOS = "pedidos.entrada.alexandre.dlq";
+    public static final String EXCHANGE = "pedidos.exchange.alexandre";
+    public static final String ROUTING_KEY = "pedidos.entrada.alexandre";
 
     @Bean
     public Queue queuePedidos() {
-        return new Queue(FILA_PEDIDOS, true);
+        return QueueBuilder.durable(FILA_PEDIDOS)
+                .withArgument("x-dead-letter-exchange", "")
+                .withArgument("x-dead-letter-routing-key", DLQ_PEDIDOS)
+                .build();
+    }
+
+    @Bean
+    public Queue deadLetterQueue() {
+        return QueueBuilder.durable(DLQ_PEDIDOS).build();
+    }
+
+    @Bean
+    public TopicExchange exchange() {
+        return new TopicExchange(EXCHANGE);
+    }
+
+    @Bean
+    public Binding pedidosBinding(Queue queuePedidos, TopicExchange exchange) {
+        return BindingBuilder.bind(queuePedidos).to(exchange).with(ROUTING_KEY);
     }
 
     @Bean
